34f5d4cd01147f194fd0ec6bad7a89e3faca7514
danny1marshall1587-maker <danny1marshall1587@gmail.com>
2026-08-31 14:21:58 +0100

    v1.1.4: Complete MIDI mode synchronization (Momentary Stomp / Hold / Latching) across native MOD and WebMIDI

--- install_cyber_tuner.py
+++ install_cyber_tuner.py
@@ -532,6 +532,92 @@ def main():
             f.write(webserver_content)
         print("  Patched mod/webserver.py successfully")
 
+
+    # 9. Patch mod/host.py to NOT remove the tuner, allowing MIDI assignments to work when it's closed!
+    print("\n9. Patching mod/host.py for MIDI stability...")
+    host_py_path = os.path.join(target_dir, 'mod', 'host.py')
+    with open(host_py_path, 'r', encoding='utf-8') as f:
+        host_py_content = f.read()
+
+    # 1. Patch kMidiLearnURI
+    old_learn = '''        # MIDI learn is not an actual addressing
+        if actuator_uri == kMidiLearnURI:
+            if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                self.send_notmodified("midi_learn %d mode %f %f" % (TUNER_INSTANCE_ID, minimum, maximum), callback, datatype='boolean')'''
+
+    new_learn = '''        # MIDI learn is not an actual addressing
+        if actuator_uri == kMidiLearnURI:
+            if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                def after_load(_):
+                    self.send_notmodified("midi_learn %d mode %f %f" % (TUNER_INSTANCE_ID, minimum, maximum), callback, datatype='boolean')
+                self.send_notmodified("add %s %d" % (TUNER_URI, TUNER_INSTANCE_ID), after_load)'''
+
+    if old_learn in host_py_content:
+        host_py_content = host_py_content.replace(old_learn, new_learn)
+
+    # 2. Patch kMidiUnlearnURI
+    old_unlearn = '''        # So we need special casing for unlearn.
+        if actuator_uri == kMidiUnlearnURI:
+            if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                self.send_modified("midi_unmap %d mode" % TUNER_INSTANCE_ID, callback, datatype='boolean')'''
+
+    new_unlearn = '''        # So we need special casing for unlearn.
+        if actuator_uri == kMidiUnlearnURI:
+            if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                def after_load(_):
+                    self.send_modified("midi_unmap %d mode" % TUNER_INSTANCE_ID, callback, datatype='boolean')
+                self.send_notmodified("add %s %d" % (TUNER_URI, TUNER_INSTANCE_ID), after_load)'''
+
+    if old_unlearn in host_py_content:
+        host_py_content = host_py_content.replace(old_unlearn, new_unlearn)
+
+    # 3. Patch midi_map
+    old_map = '''                        if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                            self.send_modified("midi_map %d mode %i %i %f %f" % (TUNER_INSTANCE_ID, channel, controller, minimum, maximum), callback, datatype='boolean')'''
+
+    new_map = '''                        if instance_id == PEDALBOARD_INSTANCE_ID and portsymbol == ":tuner":
+                            def after_load(_):
+                                self.send_modified("midi_map %d mode %i %i %f %f" % (TUNER_INSTANCE_ID, channel, controller, minimum, maximum), callback, datatype='boolean')
+                            self.send_notmodified("add %s %d" % (TUNER_URI, TUNER_INSTANCE_ID), after_load)'''
+
+    if old_map in host_py_content:
+        host_py_content = host_py_content.replace(old_map, new_map)
+
+    # 4. Patch hmi_tuner_off
+    old_off = '''    def hmi_tuner_off(self, callback):
+        logging.debug("hmi tuner off")
+
+        def tuner_removed(_):
+            if self.current_tuner_mute:
+                self.unmute()
+            callback(True)
+
+        self.send_notmodified("remove %d" % TUNER_INSTANCE_ID, tuner_removed)'''
+
+    new_off = '''    def hmi_tuner_off(self, callback):
+        logging.debug("hmi tuner off")
+
+        try:
+            hw_old_port = self.hw_tuner_input_port(self.current_tuner_port)
+            from mod.utils import disconnect_jack_ports
+            disconnect_jack_ports("system:capture_%s" % hw_old_port, "effect_%d:%s" % (TUNER_INSTANCE_ID, TUNER_INPUT_PORT))
+        except:
+            pass
+
+        def tuner_disabled(_):
+            if self.current_tuner_mute:
+                self.unmute()
+            callback(True)
+
+        self.send_notmodified("param_set %d mode 0.0" % TUNER_INSTANCE_ID, tuner_disabled)'''
+
+    if old_off in host_py_content:
+        host_py_content = host_py_content.replace(old_off, new_off)
+
+    with open(host_py_path, 'w', encoding='utf-8') as f:
+        f.write(host_py_content)
+    print("  Patched mod/host.py successfully")
+
     print("\n================================================================")
     print("  CYBER STROBE & PEAK TUNER INSTALLATION COMPLETE! 100% SUCCESS")
     print("================================================================")
